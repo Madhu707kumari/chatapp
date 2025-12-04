8c6eba86cabcbb8187c8a8c702e9ac3c667c7162
Madhu <0120madhukumari@gmail.com>
2025-12-04 20:26:41 +0530

    initial

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -15,7 +15,7 @@ function LoginPage() {
 
   return (
     <div className="w-full flex items-center justify-center p-4 bg-slate-900">
-      <div className="relative w-full max-w-6xl md:h-[750px] h-[650px]">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
         <BorderAnimatedContainer>
           <div className="w-full flex flex-col md:flex-row">
             {/* FORM CLOUMN - LEFT SIDE */}
@@ -46,7 +46,7 @@ function LoginPage() {
                     </div>
                   </div>
 
-                  {/* PASSWORD INPUT............. */}
+                  {/* PASSWORD INPUT */}
                   <div>
                     <label className="auth-input-label">Password</label>
                     <div className="relative">
